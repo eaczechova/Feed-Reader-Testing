f1507000c774b5662e60bfc3b4c96f1ebeed2773
Ewa Czech <eaprojekty@gmail.com>
2018-04-05 10:29:10 +0200

    amends click() and hasClass() methods

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -30,68 +30,35 @@ $(function() {
   });
 
   describe('The menu', function() {
-    var el = $('body');
-    var flag = true;
-
-    /* A test that ensures the menu element is hidden by default.
-    */
-
-    if(el.hasClass("menu-hidden")) {
-      flag = true;
-    } else {
-      flag = false;
-    }
-
+    const bodyElement = $('body');
     it('the menu element is hidden by default', function() {
-      expect(flag).toBe(true);
+      expect(bodyElement.hasClass('menu-hidden')).toBe(true);
     });
 
-    /* A test that ensures the menu changes visibility when
-    * the menu icon is clicked.
-    */
-
     it('the menu element changes visibility if menu icon is clicked', function() {
-      var el = $('body');
-      var menuIcon= $('menu-icon-link');
-      var flag = false;
+      const bodyElement = $('body');
+      const menuIcon= $('menu-icon-link');
 
-      /* A flag variable is used to store information about current class
-      * visibility. If body element has class .menu-hidden hidden, flag is set
-      * to true, if not, to false. In expect() function part, flag is used to
-      * validate the test.
-      */
-      
-      menuIcon.click(function() {
-        if(el.hasClass("menu-hidden")) {
-          flag = true;
-        } else {
-          flag = false;
-        }
+      expect(bodyElement.hasClass('menu-hidden')).toBe(true);
+      menuIcon.trigger('click', function() {
+        expect(bodyElement.hasClass('menu-hidden')).toBe(false);
       });
-
-      if(flag) {
-        expect(flag).toBe(true);
-      } else {
-        expect(flag).toBe(false);
-      }
-
     });
   });
 
   describe('Initial Entries', function() {
-    var container = $('.feed');
     beforeEach(function(done) {
-      loadFeed(0);
-      done();
+      loadFeed(0, function() {
+        done();
+      });
     });
 
     /* A test that ensures when the loadFeed function is called and
     * completes its work, there is at least a single .entry element
-    * within the .feed container.
     */
 
     it('feed container has at least one entry element', function() {
-      expect(container.length).not.toBe(0);
+      expect($('.entry').length).toBeGreaterThan(0);
     });
   });
 
